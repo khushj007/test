276d9b81c7172ffbdd7b50c53b0dff9928a404d0
khush <khushjoshi007@gmail.com>
2023-03-18 17:01:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const dotenv = require("dotenv")
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+const mongoose = require("mongoose");
+dotenv.config();
+const database = process.env.DATABASE
+
+
+
+
+//INITIALIZATIONS
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//SETTING MONGOOSE
+
+mongoose.connect(database);
+
+const USER_SCHEMA = mongoose.Schema({
+    fname  : String,
+    lname : String,
+})
+
+const Usercollection = mongoose.model("user",USER_SCHEMA);
+
+
+
+//SERVERS REQUESTS
+app.get("/details",(req,res)=>{
+  
+async function findData()
+{
+   const respond =  await Usercollection.find({})
+   console.log(respond);
+   res.json(JSON.stringify(respond));
+}
+findData();
+
+})
+
+app.post("/details",(req,res)=>{
+
+    const Fname  = req.body.fname ; 
+    const Lname =  req.body.lname ;
+
+    console.log
+
+    const user = new Usercollection({
+        fname : Fname,
+        lname : Lname,
+    })
+
+    user.save();
+    
+    res.json("request submitted");
+   
+})
+
+
+
+//LISTENING
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`SERVER IS RUNNING ON ${PORT}`)
+})
