8e708825232db020578158fe5788d12b08aaefc3
khush <khushjoshi007@gmail.com>
2023-03-20 10:39:34 +0530

    got it

--- server.js
+++ server.js
@@ -1,108 +1,80 @@
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require("cors");
 const mongoose = require("mongoose");
 dotenv.config();
-const database = process.env.DATABASE
-
-
-
+const database = process.env.DATABASE;
 
 //INITIALIZATIONS
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 //SETTING MONGOOSE
 
-
-mongoose.connect(database,{
-    useNewUrlParser: true ,
-    useUnifiedTopology : true ,
-}).then((res)=>{return(console.log("database connected"))}).catch((e)=>console.log("ERRORDB",e))
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    return console.log("database connected");
+  })
+  .catch((e) => console.log("ERRORDB", e));
 
 const USER_SCHEMA = mongoose.Schema({
-    fname  : String,
-    lname : String,
-})
+  fname: String,
+  lname: String,
+});
 
-const Usercollection = mongoose.model("user",USER_SCHEMA);
-
-
+const Usercollection = mongoose.model("user", USER_SCHEMA);
 
 //SERVERS REQUESTS
 
 const findData = async (req, res, next) => {
-    try {
-  
-      const tasks = await Usercollection.find({});
-  
-      res.status(200).json({
-        success: true,
-        tasks,
-      });
-    } catch (error) {
-      res.send(`there has been some error : ${error}`)
-    }
-  };
+  try {
+    const tasks = await Usercollection.find({}).maxTimeMS(30000);
 
+    res.status(200).json({
+      success: true,
+      tasks,
+    });
+  } catch (error) {
+    res.send(`there has been some error : ${error}`);
+  }
+};
 
+app.get("/detail", findData);
 
+app.post("/details", (req, res) => {
+  const Fname = req.body.fname;
+  const Lname = req.body.lname;
 
+  console.log;
 
-app.get("/detail",findData);
-  
-// async function findData()
-// {
-//     try{
-//    const respond =  await Usercollection.find({})
+  const user = new Usercollection({
+    fname: Fname,
+    lname: Lname,
+  });
 
-//    res.json(respond);
-//     }
-//     catch(err) {
-//         res.send(`there has been some err ${err}`);
-//     }
-// }
+  user.save();
 
+  res.json("request submitted");
+});
 
-app.post("/details",(req,res)=>{
-
-    const Fname  = req.body.fname ; 
-    const Lname =  req.body.lname ;
-
-    console.log
-
-    const user = new Usercollection({
-        fname : Fname,
-        lname : Lname,
-    })
-
-    user.save();
-    
-    res.json("request submitted");
-   
-})
-
-app.get("*",(req,res)=>{
-    res.send("404")
-    
-})
-
-
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 //LISTENING
 const PORT = process.env.PORT || 5000;
 
+app.use((err, req, res, next) => {
+  console.log("i runned");
+  return res.send("404 NOT FOUND");
+});
 
-app.use((err,req,res,next)=>{
-    console.log("i runned");
-    return (res.send("404 NOT FOUND"));
-})
-
-
-app.listen(PORT,()=>{
-    console.log(`SERVER IS RUNNING ON ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${PORT}`);
+});
